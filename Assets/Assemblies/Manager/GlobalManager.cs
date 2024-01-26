4721a953f1eb5a1d23a4298985fc29efe5003746
NicoIer <2320595857@qq.com>
2024-01-26 23:39:51 +0800

    feat: folder refactor & import TMP & ui template

---
+++ Assets/Assemblies/Manager/GlobalManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityToolkit;
+
+namespace GGJ2024
+{
+    public class GlobalManager : MonoSingleton<GlobalManager>
+    {
+        protected override void OnInit()
+        {
+            // UIRoot.Singleton.OpenPanel<HomePanel>();
+        }
+    }
+}
---
+++ Assets/Assemblies/UI/HomePanel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityToolkit;
+
+namespace GGJ2024
+{
+    public class HomePanel : UIPanel
+    {
+        [SerializeField, UIBind] private Button startButton;
+        private void Awake()
+        {
+            startButton.onClick.AddListener(OnStartButtonClick);
+        }
+
+        private void OnStartButtonClick()
+        {
+        }
+    }
+}
