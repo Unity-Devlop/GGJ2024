1d7aa14974edfa1b1da89cd7b0b6d5dbf2abd5f0
NicoIer <2320595857@qq.com>
2024-01-28 13:44:13 +0800

    Update StopWall.cs

--- Assets/Assemblies/GamePlay/FunctionalWall/StopWall.cs
+++ Assets/Assemblies/GamePlay/FunctionalWall/StopWall.cs
@@ -20,14 +20,13 @@ namespace GGJ2024
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (collision.gameObject.CompareTag("Player")) {
-
-                Rigidbody2D rg = collision.gameObject.GetComponent<Rigidbody2D>();
-                rg.velocity = targetVelocity;
-                _canHit = false;
-                Timer.Register(protectTime, () => { _canHit = true; });
-                AudioManager.Singleton.PlayAtCamera(GameManager.Singleton.globalConfig.stopWallClip);
-            }
+            if (!collision.gameObject.CompareTag("Player")) return;
+            if(!_canHit)return;
+            Rigidbody2D rg = collision.gameObject.GetComponent<Rigidbody2D>();
+            rg.velocity = targetVelocity;
+            _canHit = false;
+            Timer.Register(protectTime, () => { _canHit = true; });
+            AudioManager.Singleton.PlayAtCamera(GameManager.Singleton.globalConfig.stopWallClip);
         }
     }
 }
